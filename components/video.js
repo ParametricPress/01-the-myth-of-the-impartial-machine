a00df9d7a02afeae254cb4dd86e7f2c59c42b55c
Matthew Conlen <mc@mathisonian.com>
2019-03-31 21:45:46 -0700

    add video component

---
+++ components/video.js
@@ -0,0 +1,31 @@
+const React = require('react');
+
+
+const getExtension = (str) => {
+  return str.substr(str.lastIndexOf('.') + 1);
+}
+
+class Video extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
+    }
+    onChange(e) {
+        this.props.updateProps({ value: e.target.value });
+    }
+    render() {
+        const { hasError, idyll, updateProps, ...props } = this.props;
+        return (
+            <video controls={true} width={props.width} height={props.height}>
+              {
+                props.files.map((file) => {
+                  <source src={file} type={`video/${getExtension(file)}`} />
+                })
+              }
+              Your browser doesn't support embedded videos.
+          </video>
+        );
+    }
+}
+
+module.exports = Video;
