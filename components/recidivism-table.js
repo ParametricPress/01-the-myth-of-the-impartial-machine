639d8a9373fef3b2fa5cfb3edf17dff776df970d
Alice Feng <feng.a.g@gmail.com>
2019-04-12 22:15:49 -0400

    Finish building out accuracy metrics interactive

--- components/recidivism-table.js
+++ components/recidivism-table.js
@@ -1,6 +1,9 @@
 const React = require('react');
+const D3Component = require('idyll-d3-component');
+const d3 = require('d3');
 
 const accuracy = 0.666;
+const PCTFORMAT = d3.format(".0%");
 
 class RecidivismTable extends React.Component {
     constructor(props) {
@@ -8,11 +11,17 @@ class RecidivismTable extends React.Component {
         this.onChange = this.onChange.bind(this);
         this.state = {
           totalSampleSize: 100,
-          totalNoReoffense: 70,
-          totalReoffense: 30,
-          totalHighRisk: 30,
-          highRiskNoReoffense: 10,
-          highRiskReoffense: 20
+          totalNoReoffense_a: 70,
+          totalReoffense_a: 30,
+          totalHighRisk_a: 30,
+          highRiskNoReoffense_a: 10,
+          highRiskReoffense_a: 20,
+
+          totalNoReoffense_b: 40,
+          totalReoffense_b: 60,
+          totalHighRisk_b: 60,
+          highRiskNoReoffense_b: 20,
+          highRiskReoffense_b: 40
         }
     }
 
@@ -21,15 +30,15 @@ class RecidivismTable extends React.Component {
       // do some error handling here
 
       this.setState({
-        totalHighRisk: newValue,
-        highRiskNoReoffense: Math.round(accuracy * newValue),
-        highRiskReoffense: Math.round((1 - accuracy) * newValue),
+        totalHighRisk_a: newValue,
+        highRiskReoffense_a: Math.round(accuracy * newValue),
+        highRiskNoReoffense_a: Math.round((1 - accuracy) * newValue),
       })
     }
 
     render() {
         const { hasError, idyll, updateProps, ...props } = this.props;
-        const { totalSampleSize, totalNoReoffense, totalReoffense, totalHighRisk, highRiskNoReoffense, highRiskReoffense } = this.state;
+        const { totalSampleSize, totalNoReoffense_a, totalReoffense_a, totalHighRisk_a, highRiskNoReoffense_a, highRiskReoffense_a, totalNoReoffense_b, totalReoffense_b, totalHighRisk_b, highRiskNoReoffense_b, highRiskReoffense_b } = this.state;
         return (
           <div className="recidivism-tables">
             <div className = "recidivism-table groupA">
@@ -46,20 +55,20 @@ class RecidivismTable extends React.Component {
                 <tbody>
                   <tr>
                     <td>Doesn't Reoffend</td>
-                    <td>{totalNoReoffense - highRiskNoReoffense}</td>
-                    <td>{highRiskNoReoffense}</td>
-                    <td>{totalNoReoffense}</td>
+                    <td>{totalNoReoffense_a - highRiskNoReoffense_a}</td>
+                    <td>{highRiskNoReoffense_a}</td>
+                    <td>{totalNoReoffense_a}</td>
                   </tr>
                   <tr>
                     <td>Reoffends</td>
-                    <td>{totalReoffense - highRiskReoffense}</td>
-                    <td>{highRiskReoffense}</td>
-                    <td>{totalReoffense}</td>
+                    <td>{totalReoffense_a - highRiskReoffense_a}</td>
+                    <td>{highRiskReoffense_a}</td>
+                    <td>{totalReoffense_a}</td>
                   </tr>
                   <tr>
                     <td>Total</td>
-                    <td>{totalSampleSize - totalHighRisk}</td>
-                    <td><input type="text" autoComplete="off" value={totalHighRisk} onChange={this.onChange} /> </td>
+                    <td>{totalSampleSize - totalHighRisk_a}</td>
+                    <td><input type="text" autoComplete="off" value={totalHighRisk_a} onChange={this.onChange} /> </td>
                     <td>{totalSampleSize}</td>
                   </tr>
                 </tbody>
@@ -79,21 +88,49 @@ class RecidivismTable extends React.Component {
                 <tbody>
                   <tr>
                     <td>Doesn't Reoffend</td>
-                    <td>20</td>
-                    <td>20</td>
-                    <td>40</td>
+                    <td>{totalNoReoffense_b - highRiskNoReoffense_b}</td>
+                    <td>{highRiskNoReoffense_b}</td>
+                    <td>{totalNoReoffense_b}</td>
                   </tr>
                   <tr>
                     <td>Reoffends</td>
-                    <td>20</td>
-                    <td>40</td>
-                    <td>60</td>
+                    <td>{totalReoffense_b - highRiskReoffense_b}</td>
+                    <td>{highRiskReoffense_b}</td>
+                    <td>{totalReoffense_b}</td>
                   </tr>
                   <tr>
                     <td>Total</td>
-                    <td>40</td>
-                    <td>60</td>
-                    <td>100</td>
+                    <td>{totalSampleSize - totalHighRisk_b}</td>
+                    <td>{totalHighRisk_b}</td>
+                    <td>{totalSampleSize}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className = "recidivism-table accuracyMetrics">
+              <table>
+                <thead>
+                  <tr>
+                    <td></td>
+                    <td>Group A</td>
+                    <td>Group B</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Accuracy:</td>
+                    <td>{PCTFORMAT(highRiskReoffense_a/totalHighRisk_a)}</td>
+                    <td>{PCTFORMAT(highRiskReoffense_b/totalHighRisk_b)}</td>
+                  </tr>
+                  <tr>
+                    <td>False Positive Rate:</td>
+                    <td>{PCTFORMAT(highRiskNoReoffense_a/totalNoReoffense_a)}</td>
+                    <td>{PCTFORMAT(highRiskNoReoffense_b/totalNoReoffense_b)}</td>
+                  </tr>
+                  <tr>
+                    <td>False Negative Rate:</td>
+                    <td>{PCTFORMAT((totalReoffense_a - highRiskReoffense_a)/totalReoffense_a)}</td>
+                    <td>{PCTFORMAT((totalReoffense_b - highRiskReoffense_b)/totalReoffense_b)}</td>
                   </tr>
                 </tbody>
               </table>
